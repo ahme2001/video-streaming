5a6388e22901349cd51b7dafe745f5bf9818391f
ahmed <ahmedaboeleid30@gmail.com>
2026-08-24 03:29:49 +0300

    Update global handle exception

--- video/src/main/java/com/stream/video/exception/GlobalExceptionHandler.java
+++ video/src/main/java/com/stream/video/exception/GlobalExceptionHandler.java
@@ -40,11 +40,11 @@ public class GlobalExceptionHandler {
                 .body(errors);
     }
 
-    @ExceptionHandler(HlsNotReadyException.class)
-    public ResponseEntity<Map<String,String>> handleHlsNotReadyException(HlsNotReadyException e) {
+    @ExceptionHandler(PackagingNotReadyException.class)
+    public ResponseEntity<Map<String,String>> handleHlsNotReadyException(PackagingNotReadyException e) {
         Map<String,String> errors = new HashMap<>();
         errors.put("status", HttpStatus.CONFLICT.toString());
-        errors.put("hlsStatus", e.getStatus());
+        errors.put("packagingStatus", e.getStatus());
         errors.put("message", e.getMessage());
         return new ResponseEntity<>(errors, HttpStatus.CONFLICT);
     }
