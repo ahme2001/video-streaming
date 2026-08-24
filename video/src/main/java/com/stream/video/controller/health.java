d1d65cfdeaa937d346b2eaf3daa7175409c3d5d1
ahmed <ahmedaboeleid30@gmail.com>
2026-08-24 03:28:19 +0300

    Add health endpoint

--- video/src/main/java/com/stream/video/controller/health.java
+++ video/src/main/java/com/stream/video/controller/health.java
@@ -1,17 +1,15 @@
 package com.stream.video.controller;
 
-import com.stream.video.dto.VideoResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class health {
 
-    @GetMapping
+    @GetMapping("/health")
     public ResponseEntity<String> getAllVideos(){
         return ResponseEntity.
                 status(HttpStatus.OK).
